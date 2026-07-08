b01635d93d90a926da4d0675dee1b67507f36595
keven <keven@fudaoyuan.icu>
2026-07-08 12:42:28 +0800

    feat: improve loading skeleton structure and styling for better user experience

--- front/pages/s/[id].vue
+++ front/pages/s/[id].vue
@@ -40,14 +40,22 @@ const componentMap = {
 
 <template>
     <BaseCard class="my-5 overflow-hidden">
-        <div v-if="isLoading" class="flex flex-col gap-5 items-center">
-            <Skeleton class="w-20 h-5 rounded-full" />
-            <Skeleton class="w-16 h-16 rounded-xl" />
-            <Skeleton class="w-20 h-5 rounded-full" />
-            <div class="flex flex-row w-full justify-around">
-                <Skeleton class="size-10 rounded-xl" v-for="i in 3" />
+        <div v-if="isLoading" class="flex flex-col items-center gap-5">
+            <Skeleton class="h-6 w-32 rounded-md" />
+            <Skeleton class="size-16 rounded-xl" />
+            <Skeleton class="h-5 w-28 rounded-md" />
+            <div class="flex w-full flex-col gap-2 md:flex-row">
+                <div
+                    v-for="i in 3"
+                    :key="i"
+                    class="flex min-h-11 flex-1 items-center justify-between gap-1 rounded-xl bg-black/5 px-3 py-2 md:flex-col md:items-start md:justify-between"
+                >
+                    <Skeleton class="h-3 w-16 rounded-md bg-black/10" />
+                    <Skeleton v-if="i === 1" class="size-7 rounded-full bg-white/50" />
+                    <Skeleton v-else class="h-6 w-16 rounded-md bg-black/10" />
+                </div>
             </div>
-            <Skeleton class="w-full h-5 rounded-full" />
+            <Skeleton class="h-10 w-full rounded-md" />
         </div>
         <template v-else>
             <div v-if="isExpired || !data" class="flex flex-col gap-5 items-center">
